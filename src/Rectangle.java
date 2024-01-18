75032f196fde1f8c4b8d3b8b46a4509e43faa831
= <=>
2024-01-18 17:23:05 +0600

    init

---
+++ src/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    private final double a;
+    private final double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+}
